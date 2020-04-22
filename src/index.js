266836913d0422cff2f2f0b55dfcb2a68aa87b2a
Dominik Janák <dev@dominikjanak.cz>
2020-04-22 05:29:20 +0200

    Add authentication via API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Application from './Application';
 import * as serviceWorker from './serviceWorker';
+import Authentication from "./components/authentication/authentication";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Application />
+      <Authentication />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
